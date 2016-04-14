364898bc4d31449bfdb887b85154f231d75ae996
Charles Lorenzo <charles@semgeeks.com>
2016-04-14 11:02:36 -0400

    add body classes based on browser and OS

--- public/web/app/themes/worpdess/lib/extras.php
+++ public/web/app/themes/worpdess/lib/extras.php
@@ -26,6 +26,69 @@ function body_class($classes) {
     $classes[] = 'not-front';
   }
 
+  // Add classes for User Agent and Operating System
+  global $is_lynx, $is_gecko, $is_IE, $is_opera, $is_NS4, $is_safari, $is_chrome, $is_iphone;
+  
+  // Browser
+  if($is_lynx)
+  {
+    $classes[] = 'lynx';
+  } 
+  elseif($is_gecko) 
+  {
+    $classes[] = 'gecko';
+  }
+  elseif($is_opera) 
+  {
+    $classes[] = 'opera';
+  }
+  elseif($is_NS4) 
+  {
+    $classes[] = 'ns4';
+  }
+  elseif($is_safari) 
+  {
+    $classes[] = 'safari';
+  }
+  elseif($is_chrome) 
+  {
+    $classes[] = 'chrome';
+  }
+  elseif($is_IE) 
+  {
+    $classes[] = 'ie';
+          
+    // IE Version Number
+    if(preg_match('/MSIE ([0-9]+)([a-zA-Z0-9.]+)/', $_SERVER['HTTP_USER_AGENT'], $browser_version))
+    {
+      $classes[] = 'ie'.$browser_version[1];
+    }
+  } 
+  else
+  {
+    $classes[] = 'unknown';
+  }
+
+  // Iphone
+  if($is_iphone) 
+  {
+    $classes[] = 'iphone';
+  }
+
+  // Operating System
+  if(stristr($_SERVER['HTTP_USER_AGENT'],"mac")) 
+  {
+    $classes[] = 'osx';
+  } 
+  elseif(stristr($_SERVER['HTTP_USER_AGENT'],"linux")) 
+  {
+    $classes[] = 'linux';
+  } 
+  elseif(stristr($_SERVER['HTTP_USER_AGENT'],"windows")) 
+  {
+    $classes[] = 'windows';
+  }
+
   return $classes;
 }
 add_filter('body_class', __NAMESPACE__ . '\\body_class');
