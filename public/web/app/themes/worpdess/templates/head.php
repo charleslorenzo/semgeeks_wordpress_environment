76c8b64a3c12e5c440a1eb8577173e948fe4503c
John Berry <jpberry0623@gmail.com>
2016-08-24 13:02:10 -0400

    updated head file to point to semgeeks favicon in theme directory(to be replaced by clients favicon prior to launch)

--- public/web/app/themes/worpdess/templates/head.php
+++ public/web/app/themes/worpdess/templates/head.php
@@ -2,8 +2,8 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="icon" href="<?php bloginfo('url'); ?>/favicon.ico" type="image/x-icon" />
-  <link rel="shortcut icon" href="<?php bloginfo('url'); ?>/favicon.ico" type="image/x-icon" />
+  <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico" type="image/x-icon" />
+  <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico" type="image/x-icon" />
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- Typekit Scripts here -->
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
